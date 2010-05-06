c7011a228abc3332b11549a515647e9b66e58491
salons <salons@2fa0627c-b022-0410-acda-9783b4e515f1>
2010-05-06 12:15:17 +0000

    cameraserver now supports firewire cameras

--- branches/5.0/m4/component_cameraserver.m4
+++ branches/5.0/m4/component_cameraserver.m4
@@ -1,5 +1,6 @@
 dnl # Requirements for component cameraserver
 dnl # GStreamer
+dnl # Firewire
 
 AC_ARG_ENABLE([component-cameraserver],
     [AS_HELP_STRING([--disable-component-cameraserver],
@@ -12,6 +13,9 @@ if test "x$enable_component_cameraserver" != xno; then
     AC_MSG_NOTICE([**** Checking cameraserver component requirements:])
     if test "x$with_gstreamer" = xno; then
 	ERRORS="$ERRORS, gstreamer support not found. Try setting --with-gstreamer"
+    fi
+    if test "x$ENABLED_LIB1394" != xyes; then
+	ERRORS="$ERRORS, libdc1394 support not found."
     fi
     if test "$ERRORS"; then
         AC_MSG_NOTICE([Errors found checking cameraserver requirements: $ERRORS. Component disabled])
--- branches/5.0/src/components/cameraserver/cameraserver.cpp
+++ branches/5.0/src/components/cameraserver/cameraserver.cpp
@@ -32,9 +32,13 @@
 #include <jderobotice/application.h>
 #include <tr1/memory>
 #include <list>
+#include <string.h>
 #include <sstream>
 #include "gstpipeline.h"
 #include <stdlib.h>
+#include <inttypes.h>
+#include <libraw1394/raw1394.h>
+#include <dc1394/control.h>
 
 namespace cameraserver{
   class CameraI: virtual public jderobot::Camera {
@@ -46,7 +50,9 @@ namespace cameraserver{
 	cameraDescription(new jderobot::CameraDescription()),
 	imageConsumer(),
 	replyTask(),
-	rpc_mode(false)
+	rpc_mode(false),
+	camera1394(),
+	firewire_mode(false)
     {
       
       
@@ -82,9 +88,15 @@ namespace cameraserver{
 
       //pipelineCfg.validate();FIXME: validate cfg before to use it
 
-
-      context.tracer().info("Creating pipeline with config: " + pipelineCfg.toString());
-      pipeline = new GSTPipeline(context,pipelineCfg);
+      if(strncmp((char*)pipelineCfg.uri.c_str(),"dv",2)==0){
+	firewire_mode=true;
+	context.tracer().info("Creating firewire thread with config: " + pipelineCfg.toString());
+      }
+      else{
+      	context.tracer().info("Creating pipeline with config: " + pipelineCfg.toString());
+      	pipeline = new GSTPipeline(context,pipelineCfg);
+      }
+      
       context.tracer().info("Starting thread for camera: " + cameraDescription->name);
       replyTask = new ReplyTask(this);
 
@@ -121,6 +133,9 @@ namespace cameraserver{
 
     virtual ~CameraI(){
       context.tracer().info("Stopping pipeline");
+      if(firewire_mode){
+	cleanup(camera1394);
+      }
       pipeline->stop();
       context.tracer().info("Stopping and joining thread for camera: " + cameraDescription->name);
       gbxiceutilacfr::stopAndJoin(replyTask);
@@ -164,74 +179,228 @@ namespace cameraserver{
     	return;
     }
 
-
   private:
-    class ReplyTask: public gbxiceutilacfr::SafeThread{
-    public:
-      ReplyTask(CameraI* camera)
-	: gbxiceutilacfr::SafeThread(camera->context.tracer()), mycamera(camera) {
-	start();//start thread
-      }
+	/** cleans up firewire structures and frees the firewire bus.*/
+	int cleanup(dc1394camera_t *camera) {
+	    dc1394_video_set_transmission(camera, DC1394_OFF);
+	    dc1394_capture_stop(camera);
+	    return 0;
+	}
 
-      void pushJob(const jderobot::AMD_ImageProvider_getImageDataPtr& cb){
-	IceUtil::Mutex::Lock sync(requestsMutex);
-	requests.push_back(cb);
-      }
+	/** firewire driver init function. It will start all firewire required devices
+	 *  and setting them the default configuration.
+	 *  @return 0 if initialitation was successful or -1 if something went wrong.*/
+	void firewire_deviceinit(){
+		const int NUM_BUFFERS=1;
+		dc1394_t * d;  
+		dc1394error_t err;
+        	dc1394video_mode_t res;
+        	dc1394framerate_t f;
+		dc1394camera_list_t *list;
+		char device[128];
+		strncpy(device,pipelineCfg.uri.c_str(),pipelineCfg.uri.length());
+		device[pipelineCfg.uri.length()]='\0';
 
-      virtual void walk(){
-	jderobot::ImageDataPtr reply(new jderobot::ImageData);
-	reply->description = mycamera->imageDescription;
-	
-	while(!isStopping()){
-	  GstBuffer* buff = mycamera->pipeline->pull_buffer();
-	  if (!buff){
-	    mycamera->context.tracer().info("Pipeline return empty buffer. Waiting...");
-	    IceUtil::ThreadControl::sleep(IceUtil::Time::seconds(1));
-	  }else{
-	    IceUtil::Time t = IceUtil::Time::now();
-	    reply->timeStamp.seconds = (long)t.toSeconds();
-	    reply->timeStamp.useconds = (long)t.toMicroSeconds() - reply->timeStamp.seconds*1000000;
-	    reply->pixelData.resize(mycamera->imageDescription->size);
-	    memmove( &(reply->pixelData[0]), (char*)buff->data, mycamera->imageDescription->size);//copy data to reply
-	    gst_buffer_unref(buff);//release gstreamer buffer
+		// camera config
+		int width=imageDescription->width;
+		int fps=pipelineCfg.framerateN;
+		int camera_id=atoi(&device[strlen(device)-1]);
+	   
+		list=NULL;
+		d = dc1394_new ();
+		err=dc1394_camera_enumerate (d, &list);
 
-	    // publish
-	    if(mycamera->imageConsumer!=0){
-		mycamera->imageConsumer->report(reply);
-	    }
+		if (list == NULL) {
+			perror("Can't access firewire device\n");
+		}
+	  
+		camera1394 = dc1394_camera_new (d, list->ids[camera_id].guid);
+		if (!camera1394){
+			dc1394_log_warning("Failed to initialize camera with id %d",camera_id);
+		}
 
-	    // response to data petition
-	    if(mycamera->rpc_mode){
-	    	{//critical region start
-	      	IceUtil::Mutex::Lock sync(requestsMutex);
-	      	while(!requests.empty()){
-			jderobot::AMD_ImageProvider_getImageDataPtr cb = requests.front();
-			requests.pop_front();
-			cb->ice_response(reply);
-	      	}
-	    	}//critical region end
-	    }
+		if(width==320){
+			res = DC1394_VIDEO_MODE_320x240_YUV422;
+		}
+		else{
+		// This resolution is only compatible with framerate 15
+			res = DC1394_VIDEO_MODE_640x480_YUV422;
+		}
 
+
+		if(fps==30){
+			f = DC1394_FRAMERATE_30;
+		}
+		else if(fps==15){
+			f = DC1394_FRAMERATE_15;
+		}
+		else{
+			f = DC1394_FRAMERATE_15;
+			fprintf(stderr,"Wrong framerate for camera with id %d, set to default 15 fps\n",camera_id);
+		}
+
+		err=dc1394_video_set_iso_speed(camera1394, DC1394_ISO_SPEED_400);
+		//DC1394_ERR_CLN_RTN(err,cleanup(camera),"Could not set ISO speed");
+
+		err=dc1394_video_set_mode(camera1394, res);
+		//DC1394_ERR_CLN_RTN(err,cleanup(camera),"Could not set video mode");
+
+		err=dc1394_video_set_framerate(camera1394, f);
+		//DC1394_ERR_CLN_RTN(err,cleanup(camera),"Could not set framerate");
+
+		err=dc1394_capture_setup(camera1394,NUM_BUFFERS, DC1394_CAPTURE_FLAGS_DEFAULT);
+		//DC1394_ERR_CLN_RTN(err,cleanup(camera),"Could not setup camera-\nmake sure that the video mode and framerate are\nsupported by your camera");
+
+		err=dc1394_video_set_transmission(camera1394, DC1394_ON);
+		//DC1394_ERR_CLN_RTN(err,cleanup(camera),"Could not start camera iso transmission");
+
+	    	dc1394_camera_free_list (list);
+	}
+
+	/** function to transform a buffer from uyvy to rgb.
+	 *  @param src source buffer.
+	 *  @param dest destination buffer where the transformation will be set.
+	 *  @param NumPixels how many pixels per buffer.*/
+	void uyvy2rgb (unsigned char *src, unsigned char *dest, unsigned long long int NumPixels)
+	{
+	/**
+	 * Color conversion functions from Bart Nabbe.
+	 * Corrected by Damien: bad coeficients in YUV2RGB.
+	 */
+	#define YUV2RGB(y, u, v, r, g, b)		\
+	  r = y + ((v*1436) >> 10);			\
+	  g = y - ((u*352 + v*731) >> 10);		\
+	  b = y + ((u*1814) >> 10);			\
+	  r = r < 0 ? 0 : r;				\
+	  g = g < 0 ? 0 : g;				\
+	  b = b < 0 ? 0 : b;				\
+	  r = r > 255 ? 255 : r;			\
+	  g = g > 255 ? 255 : g;			\
+	  b = b > 255 ? 255 : b
+
+	  register int i = (NumPixels << 1)-1;
+	  register int j = NumPixels + ( NumPixels << 1 ) -1;
+	  register int y0, y1, u, v;
+	  register int r, g, b;
+
+	  while (i > 0) {
+	    y1 = (unsigned char) src[i--];
+	    v  = (unsigned char) src[i--] - 128;
+	    y0 = (unsigned char) src[i--];
+	    u  = (unsigned char) src[i--] - 128;
+	    YUV2RGB (y1, u, v, b, g, r);
+	    dest[j--] = r;
+	    dest[j--] = g;
+	    dest[j--] = b;
+	    YUV2RGB (y0, u, v, b, g, r);
+	    dest[j--] = r;
+	    dest[j--] = g;
+	    dest[j--] = b;
 	  }
 	}
-      }
-      CameraI* mycamera;
-      IceUtil::Mutex requestsMutex;
-      std::list<jderobot::AMD_ImageProvider_getImageDataPtr> requests;
-    };
-    typedef IceUtil::Handle<ReplyTask> ReplyTaskPtr;
+
+    	class ReplyTask: public gbxiceutilacfr::SafeThread{
+    	public:
+	      ReplyTask(CameraI* camera)
+		: gbxiceutilacfr::SafeThread(camera->context.tracer()), mycamera(camera) {
+		start();//start thread
+	      }
+
+	      void pushJob(const jderobot::AMD_ImageProvider_getImageDataPtr& cb){
+		IceUtil::Mutex::Lock sync(requestsMutex);
+		requests.push_back(cb);
+	      }
+
+	      virtual void walk(){
+		jderobot::ImageDataPtr reply(new jderobot::ImageData);
+		reply->description = mycamera->imageDescription;
+
+		if(mycamera->firewire_mode){
+			mycamera->firewire_deviceinit();
+		}
+	
+		bool new_frame;
+		GstBuffer* buff;
+		dc1394video_frame_t *frame;
+		dc1394camera_t *camera=mycamera->camera1394;
+
+		while(!isStopping()){
+		  new_frame=true;
+		  if(mycamera->firewire_mode){
+			if (dc1394_capture_dequeue(camera, DC1394_CAPTURE_POLICY_WAIT, &frame)!=DC1394_SUCCESS){
+				dc1394_log_error("Failed to capture from firewire camera");  
+				new_frame=false;  
+			}
+		  }
+		  else{
+		  	buff = mycamera->pipeline->pull_buffer();
+			if(!buff){
+				new_frame=false;
+		    		mycamera->context.tracer().info("Pipeline return empty buffer. Waiting...");
+		    		IceUtil::ThreadControl::sleep(IceUtil::Time::seconds(1));
+			}
+		  }
+
+		  if (new_frame){
+		    	IceUtil::Time t = IceUtil::Time::now();
+		    	reply->timeStamp.seconds = (long)t.toSeconds();
+		    	reply->timeStamp.useconds = (long)t.toMicroSeconds() - reply->timeStamp.seconds*1000000;
+		    	reply->pixelData.resize(mycamera->imageDescription->width*mycamera->imageDescription->height*3);
+
+		    	if(mycamera->firewire_mode){
+			    dc1394_capture_enqueue (camera, frame);
+
+			    if (mycamera->imageFmt == colorspaces::ImageRGB888::FORMAT_RGB888){
+				mycamera->uyvy2rgb((unsigned char*)frame->image,&(reply->pixelData[0]),frame->size[0]*frame->size[1]);
+			    }
+			    else{
+			    // Format colorspaces::ImageRGB888::FORMAT_YUYV
+				memmove( &(reply->pixelData[0]),frame->image, mycamera->imageDescription->size);
+			    }
+		    	}
+		    	else{
+		    	    memmove( &(reply->pixelData[0]), (char*)buff->data, mycamera->imageDescription->size);//copy data to reply
+		    	    gst_buffer_unref(buff);//release gstreamer buffer
+		    	}
+
+		    	// publish
+		    	if(mycamera->imageConsumer!=0){
+			    mycamera->imageConsumer->report(reply);
+		    	}
+
+		    	// response to data petition
+		    	if(mycamera->rpc_mode){
+		    	    {//critical region start
+		      	    IceUtil::Mutex::Lock sync(requestsMutex);
+		      	    while(!requests.empty()){
+				jderobot::AMD_ImageProvider_getImageDataPtr cb = requests.front();
+				requests.pop_front();
+				cb->ice_response(reply);
+		      	    }
+		    	    }//critical region end
+		        }
+		  }
+		}
+	      }
+	      CameraI* mycamera;
+	      IceUtil::Mutex requestsMutex;
+	      std::list<jderobot::AMD_ImageProvider_getImageDataPtr> requests;
+	    };
+	    typedef IceUtil::Handle<ReplyTask> ReplyTaskPtr;
 
 
-    std::string prefix;
-    jderobotice::Context context;
-    GSTPipelinePtr pipeline;
-    Config pipelineCfg;
-    colorspaces::Image::FormatPtr imageFmt;
-    jderobot::ImageDescriptionPtr imageDescription;
-    jderobot::CameraDescriptionPtr cameraDescription;
-    ReplyTaskPtr replyTask;
-    jderobot::ImageConsumerPrx imageConsumer;
-    bool rpc_mode;
+	    std::string prefix;
+	    jderobotice::Context context;
+	    GSTPipelinePtr pipeline;
+	    Config pipelineCfg;
+	    colorspaces::Image::FormatPtr imageFmt;
+	    jderobot::ImageDescriptionPtr imageDescription;
+	    jderobot::CameraDescriptionPtr cameraDescription;
+	    ReplyTaskPtr replyTask;
+	    jderobot::ImageConsumerPrx imageConsumer;
+	    dc1394camera_t *camera1394;
+	    bool rpc_mode;
+	    bool firewire_mode;
   };
 
 
