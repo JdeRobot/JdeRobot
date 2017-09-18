b77427e63bfe8a4e222ecf2a114d343ee013a2fe
Francisco <f.perez475@gmail.com>
2017-09-18 13:31:15 -0700

    [issue #882] New version of colorTuner

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/fran/fqez/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/fran/fqez/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/fran/fqez/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/fran/fqez/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/fran/fqez:$ROS_PACKAGE_PATH"
---
+++ build/src/libs/jderobotcomm_cpp/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/fran/fqez/src/libs/jderobotcomm_cpp/include".split(';') if "/home/fran/fqez/src/libs/jderobotcomm_cpp/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;message_runtime".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ljderobotcomm".split(';') if "-ljderobotcomm" != "" else []
+PROJECT_NAME = "jderobotcomm"
+PROJECT_SPACE_DIR = "/home/fran/fqez/build/devel"
+PROJECT_VERSION = "0.0.1"
