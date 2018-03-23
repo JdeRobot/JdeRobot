6983a94eb89f29aa0fb15b578d716eb8fd365d7f
Abhay <abhayrawat2695@gmail.com>
2018-03-23 20:24:26 +0530

    #1107 stream from ROS topics can now be veiwed

--- src/tools/cameraview_py/cameraview.py
+++ src/tools/cameraview_py/cameraview.py
@@ -1,29 +1,30 @@
-import sys, traceback, Ice
-import easyiceconfig as EasyIce
-import jderobot
-import numpy as np
+import sys
 import threading
-
+import config
+import comm
 import cv2
+from comm.ros.listenerCamera import ListenerCamera
 
-
-if __name__ == "__main__":
-    ic = EasyIce.initialize(sys.argv)
-    properties = ic.getProperties()
-    basecameraL = ic.propertyToProxy("Camera.Proxy")
-    cameraProxy = jderobot.CameraPrx.checkedCast(basecameraL)
-
-    key=-1
-    while key != 1048689:
-        imageData = cameraProxy.getImageData("RGB8")
-        imageData_h = imageData.description.height
-        imageData_w = imageData.description.width
-        image = np.zeros((imageData_h, imageData_w, 3), np.uint8)
-        image = np.frombuffer(imageData.pixelData, dtype=np.uint8)
-        image.shape = imageData_h, imageData_w, 3
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        cv2.imshow("Image", image)
+def showImage(client):
+    key = -1
+    while key not in [13, 27]:
+        imageData =  client.getImage()
+        cv2.imshow("Image", cv2.cvtColor(imageData.data, cv2.COLOR_RGB2BGR))
         key=cv2.waitKey(30)
 
+if __name__ == "__main__":
+    try:
+        cfg = config.load(sys.argv[1])
+        Server = cfg.getProperty("Cameraview.Camera.Server")
+        rosserver = comm.server2int(Server) == 2
+        jdrc = comm.init(cfg, "Cameraview")
+        client = jdrc.getCameraClient("Cameraview.Camera")
+        showImage(client)
 
+    except IndexError:
+        print("Please pass the configuration file.")
+        sys.exit(1)
+    except KeyError as e:
+        print("Key Error in config file for {}".format(e))
+        sys.exit(1)
 
